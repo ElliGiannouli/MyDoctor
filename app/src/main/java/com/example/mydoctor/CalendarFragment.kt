704d20cb258dcbbe71669116403fa8f77fd7f7b0
DimitrisSidiropoulos <94233147+DimitrisSidiropoulos@users.noreply.github.com>
2021-11-24 22:54:02 +0200

    activity_register.xml : added input type for password and AMKA. fragment_calendar.xml : added dropdown list prototype strings.xml : added the "choose a date"string for the drop down list.

--- app/src/main/java/com/example/mydoctor/CalendarFragment.kt
+++ app/src/main/java/com/example/mydoctor/CalendarFragment.kt
@@ -5,14 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import com.example.mydoctor.databinding.ActivityMainBinding
+import com.example.mydoctor.databinding.FragmentCalendarBinding
+
 class CalendarFragment : Fragment() {
 
+    private lateinit var binding: FragmentCalendarBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        super.onCreate(savedInstanceState)
+        binding.FragmentCalendarBinding.inflate(layoutInflater)
+
+        val items = listOf("Option 1", "Option 2", "Option 3", "Option 4")
+        val adapter_date = ArrayAdapter(requireContext(), R.layout.list_item, items)
+        binding.autocompleteTextViewDateDropdown.setAdapter(adapter_date)
+
         return inflater.inflate(R.layout.fragment_calendar, container, false)
     }
+
+
+
 
 }
