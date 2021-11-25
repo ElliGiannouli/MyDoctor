8a591022878fe196bdbb91b1de5579168275bc5e
DimitrisSidiropoulos <94233147+DimitrisSidiropoulos@users.noreply.github.com>
2021-11-25 20:00:57 +0200

    Created fragment_calendar_2.xml with LinearLayout instead of FrameLayout. Other minor changes.

--- app/src/main/java/com/example/mydoctor/CalendarFragment.kt
+++ app/src/main/java/com/example/mydoctor/CalendarFragment.kt
@@ -8,11 +8,12 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import com.example.mydoctor.R.layout.list_dates
 import com.example.mydoctor.R.layout.list_times
+import com.example.mydoctor.databinding.FragmentCalendar2Binding
 import com.example.mydoctor.databinding.FragmentCalendarBinding
 
 class CalendarFragment : Fragment() {
 
-    private lateinit var binding: FragmentCalendarBinding
+    private lateinit var binding: FragmentCalendar2Binding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,17 +21,17 @@ class CalendarFragment : Fragment() {
     ): View? {
 
         super.onCreate(savedInstanceState)
-        binding = FragmentCalendarBinding.inflate(layoutInflater)
+        binding = FragmentCalendar2Binding.inflate(layoutInflater)
 
         val itemsDates = listOf("December 13 2021", "December 14 2021", "December 15 2021", "December 16 2021","December 17 2021")
-        val adapterDate = ArrayAdapter(requireContext(), list_dates, itemsDates)
+        val adapterDate = ArrayAdapter(requireContext(),list_dates, itemsDates)
         binding.autocompleteTextViewDateDropdown.setAdapter(adapterDate)
 
         val itemsTimes = listOf("10:00","11:00","12:00","13:00","14:00","17:30","18:30","19:30","20:30")
         val adapterTime = ArrayAdapter(requireContext(),list_times,itemsTimes)
         binding.autocompleteTextViewTimeDropdown.setAdapter(adapterTime)
 
-        return inflater.inflate(R.layout.fragment_calendar, container, false)
+        return inflater.inflate(R.layout.fragment_calendar_2, container, false)
     }
 
 
--- app/src/main/java/com/example/mydoctor/ProfileFragment.kt
+++ app/src/main/java/com/example/mydoctor/ProfileFragment.kt
@@ -5,9 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.example.mydoctor.databinding.FragmentCalendar2Binding
 
 class ProfileFragment : Fragment() {
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,3 +18,4 @@ class ProfileFragment : Fragment() {
     }
 
 }
+
