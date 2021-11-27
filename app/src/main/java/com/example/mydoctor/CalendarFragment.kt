db280bd560f374debf26a7d0d89d7c77c81a1701
DimitrisSidiropoulos <94233147+DimitrisSidiropoulos@users.noreply.github.com>
2021-11-27 15:30:16 +0200

    Created ConfirmationFragment.kt and added nav_graph.xml

--- app/src/main/java/com/example/mydoctor/CalendarFragment.kt
+++ app/src/main/java/com/example/mydoctor/CalendarFragment.kt
@@ -1,19 +1,37 @@
 package com.example.mydoctor
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.example.mydoctor.R.layout.list_dates
 import com.example.mydoctor.R.layout.list_times
 import com.example.mydoctor.databinding.FragmentCalendar2Binding
 import com.example.mydoctor.databinding.FragmentCalendarBinding
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import kotlin.text.Typography.times
+
 
 class CalendarFragment : Fragment() {
 
     private lateinit var binding: FragmentCalendar2Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setHasOptionsMenu(true)
+
+//        val bookDateButton = binding.bookADateButton as TextView
+//        bookDateButton.setOnClickListener(){
+//            val intent = Intent(this, ConfirmationFragment::class.java);
+//            startActivity(intent)
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,15 +41,16 @@ class CalendarFragment : Fragment() {
         super.onCreate(savedInstanceState)
         binding = FragmentCalendar2Binding.inflate(layoutInflater)
 
-        val itemsDates = listOf("December 13 2021", "December 14 2021", "December 15 2021", "December 16 2021","December 17 2021")
+        val itemsDates = resources.getStringArray(R.array.dates)
         val adapterDate = ArrayAdapter(requireContext(),list_dates, itemsDates)
         binding.autocompleteTextViewDateDropdown.setAdapter(adapterDate)
 
-        val itemsTimes = listOf("10:00","11:00","12:00","13:00","14:00","17:30","18:30","19:30","20:30")
+        val itemsTimes = resources.getStringArray(R.array.times)
         val adapterTime = ArrayAdapter(requireContext(),list_times,itemsTimes)
         binding.autocompleteTextViewTimeDropdown.setAdapter(adapterTime)
 
-        return inflater.inflate(R.layout.fragment_calendar_2, container, false)
+        return binding.root
+
     }
 
 
